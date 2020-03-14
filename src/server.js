e634d0765458c993cdc5f3f76f9e10fee1d47e62
Nazeh-Taha <nazeh3036@gmail.com>
2020-03-14 23:56:30 +0200

    make it better

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 
-require("dotenv/config");
+require('dotenv').config();
 
 //Middleware
 app.use(express.static(path.join(__dirname, "/build")));
